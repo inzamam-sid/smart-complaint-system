7436a9401ebecb4067a149861dc5974d59501d96
frazhaidry <frazhaidry24@gmail.com>
2025-05-19 20:59:32 +0530

    Testing Fork

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <Navbar role="student" />
       <div className="p-4">
         <h1 className="text-3xl font-bold">Welcome to Smart Complaint System</h1>
+        <h2>Hello guyz you know what time it is ?</h2>
       </div>
     </>
   );
